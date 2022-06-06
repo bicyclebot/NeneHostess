119689bc28446d8d12aa79980517daae7e957ca3
aren | bicycle <106108886+bicyclebot@users.noreply.github.com>
2022-06-06 19:57:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,36 +65,50 @@ client.on('ready', async () => {
 client.on('message', async msg => {
     try {
 
-        // Extract content for easier manipulation
-        let message = msg.content;
-	  return;
+      // Extract content for easier manipulation
+      let message = msg.content;
 
-    // Check if command for the bot
-	if (message.startsWith(prefix)) {
+      // Check if command for the bot
+      if (message.startsWith(prefix)) {
 
-		// Remove command indicatior prefix and split into args and command
-		let args = message.substring(prefix.length).split(' ');
-		let cmd = args[0];
-		args = args.splice(1);
+        // Remove command indicatior prefix and split into args and command
+        let args = message.substring(prefix.length).split(' ');
+        let cmd = args[0];
+        args = args.splice(1);
 
-		// Parse Command
-		switch(cmd.toLowerCase()) {
-			case 'genin5x': // sends a message to a given channel
-				ts = Math.round((new Date()).getTime() / 1000);
-				msg.reply('Hit ``Ready`` in <t:${ts+5}:R>!');
-				messageChannelX.send('Lobby going up in <t:${ts+5}:R>!');
-				break;
-			case 'genin5y': // sends a message to a given channel
-				ts = Math.round((new Date()).getTime() / 1000);
-				msg.reply('Hit ``Ready`` in <t:${ts+5}:R>!');
-				messageChannelY.send('Lobby going up in <t:${ts+5}:R>!');
-				break;
-
+        // Parse Command
+        switch(cmd.toLowerCase()) {
+            case 'genin5x': // sends a message to a given channel
+                ts = Math.round((new Date()).getTime() / 1000);
+                msg.reply(`Hit **__Ready__** in <t:${ts+5}:R>!`);
+                messageChannelX.send(`Lobby going up in <t:${ts+5}:R>!`);
+                break;
+            case 'genin5y': // sends a message to a given channel
+                ts = Math.round((new Date()).getTime() / 1000);
+                msg.reply(`Hit **__Ready__** in <t:${ts+5}:R>!`);
+                messageChannelY.send('Lobby going up in <t:${ts+5}:R>!');
+                break;
+        }
+      }
+    }
+// Error Message
+    catch {
+        catch (err) {
+        let time = "";
+        try { time = new Date().toGMTString(); }
+        catch (er) { }
+        let errmess = time + ":\n" + err.stack + "\n\n";
+        fs.appendFile(errorFile, errmess, (error) => {
+            // If there's a problem writing errors, just silently suffer
+        });
+            console.log(errmess);
+            try { msg.channel.send("Oh, I don't feel so good."); }
+            catch (er) { }
         } // End Switch
-	} // End if
+    } // End if
   }
 				
 if(useAuthFile)
 	client.login(auth.token);
 else
-	client.login(process.env.BOT_TOKEN);			
+	client.login(process.env.BOT_TOKEN);		
